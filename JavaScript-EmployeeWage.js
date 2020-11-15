f8642cdbdb43759a39763ba752e9314abed8e143
kretika arora <kretikaarora@gmail.com>
2020-11-15 14:32:14 +0530

    [Kretika Arora][Refractor]Writing Function To Calculate Daily Wage For Parttime and Fulltime

--- JavaScript-EmployeeWage.js
+++ JavaScript-EmployeeWage.js
@@ -13,14 +13,14 @@ else
     console.log("Employee is present");
 }
 }
-//UC2-To Check PartTime and FullTime Wage
-console.log("uc2-PartTime or fulltime wage")
+//UC2-To Check PartTime and FullTime WageS
+console.log("UC2-PartTime or fulltime wage")
 const IS_Full_TIME=2;
 const IS_PART_TIME=1;
 const WAGE_PER_HOUR=20;
 let emp_hours=0;
-let checkForEmployeePresence= Math.floor((Math.random()*10)%3);
-switch(checkForEmployeePresence)
+let empCheck= Math.floor((Math.random()*10)%3);
+switch(empCheck)
 {
     case 1:       
         emp_hours=4;
@@ -33,4 +33,31 @@ switch(checkForEmployeePresence)
         break;
 }
 let employeeWage= WAGE_PER_HOUR*emp_hours;
-console.log("empWage : "+employeeWage);
+console.log("empWage for UC2 : "+employeeWage);
+{
+    //UC3-Refractor UC2 to Write Functions
+    console.log("UC3-Refractor UC2 to Write Functions");  
+    const IS_Full_TIME=2;
+    const IS_PART_TIME=1;
+    const Full_TIME_HRS=8;
+    const PART_TIME_HRS=4;
+    const WAGE_PER_HOUR=20;
+    function getWorkingHrs(empCheck)
+    {
+        switch(empCheck)
+        {
+            case 1:       
+                return PART_TIME_HRS;
+                
+            case 2:      
+                 return Full_TIME_HRS;           
+            default:
+                return 0;        
+    
+        }
+    }
+    let empCheck= Math.floor((Math.random()*10)%3);
+    let empHrs=getWorkingHrs(empCheck);
+    let employeeWage= WAGE_PER_HOUR*emp_hours;
+    console.log("empWage For UC3: "+employeeWage);
+}
